fbc133ed527c9e5150b72ef6ebc23fad74152ce6
Emma <zuluagamoraemmanuel@gmail.com>
2026-03-12 12:26:35 -0500

    fix: @Builder.Default notation added to UserProfile in streaks variable. It fixes inconsistency

--- gestor/src/main/java/com/myapp/gestor/model/UserProfile.java
+++ gestor/src/main/java/com/myapp/gestor/model/UserProfile.java
@@ -18,7 +18,7 @@ public class UserProfile {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Builder.Default
     private Integer streaks = 0;
 
     @NotBlank(message = "The currency must be selected")
